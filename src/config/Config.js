aafaee0ad183b7b45ca31e8b750cfdcb06d27b67
BaoXuebin <baoxbin@hotmail.com>
2018-10-14 16:16:31 +0800

    修改主题

---
+++ src/config/Config.js
@@ -0,0 +1,7 @@
+const DefaultConfig = require('./Default-Config');
+
+module.exports = Object.assign(DefaultConfig, {
+    title: "后台管理",
+    copyright: "@2018 xdbin.com",
+    theme: "#1DA57A"
+});
