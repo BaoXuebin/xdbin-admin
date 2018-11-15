80a7828cfdc8629ab8d043f23db9f2baebb9cc16
BaoXuebin <baoxbin@hotmail.com>
2018-11-15 22:25:07 +0800

    alter api path

--- src/api/ManaReq.js
+++ src/api/ManaReq.js
@@ -2,13 +2,13 @@ import { fetch } from './Req';
 
 export const defaultHandleError = (err) => { console.error(err); };
 
-export const loginReq = ({ username, password }) => fetch('/login', {
+export const loginReq = ({ username, password }) => fetch('/api/v1/login', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password })
 });
 
-export const validateReq = () => fetch('/validate', {
+export const validateReq = () => fetch('/api/v1/validate', {
     method: 'post',
     headers: { 'Content-Type': 'application/json' }
 });
